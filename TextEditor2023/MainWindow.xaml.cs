135a29ed9d44d2014c977e9474d14271daffe38f
Maria Edström <mariat.edstrom@gmail.com>
2023-10-18 10:49:44 +0200

    Save and Open file complete

--- TextEditor2023/MainWindow.xaml.cs
+++ TextEditor2023/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Path = System.IO.Path;
 
 namespace TextEditor2023
 {
@@ -20,14 +22,43 @@ namespace TextEditor2023
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Create filePath as field
+        string _filePath = string.Empty;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Create folder
+            var directoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TextEditorPath2023");
+            Directory.CreateDirectory(directoryPath);
+
+            // Edit file, will overwrite existing 
+            _filePath = Path.Combine(directoryPath, "test.txt");
+        }
+
+        private void SaveFileBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            // Save input text 
+            var mainMenuText = MainTextBox.Text;
+
+            // Save to file
+            // using, no need to close StreamWriter 
+            using var sw = new StreamWriter(_filePath);
+            sw.Write(mainMenuText);
         }
 
         private void OpenFileBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            // Read file 
+            using var sr = new StreamReader(_filePath);
+            // Read the entire file at once
+            var text = sr.ReadToEnd();
+
+            // Display text in MainTextBox
+            // text = property {get; set;}
+            MainTextBox.Text = text;
+
         }
     }
 }
